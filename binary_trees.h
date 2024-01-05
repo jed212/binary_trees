c1056dd2c5b8591f25808423b8f57935b45f7fa7
jedidiah <jedidahfavour@gmail.com>
2024-01-06 00:43:52 +0300

    binary tree operations

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,12 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
+typedef struct levelorder_queue
+{
+        struct binary_tree_s *node;
+        struct levelorder_queue *next;
+} levelorder_queue_t;
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -46,4 +52,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+levelorder_queue_t *create_node(binary_tree_t *node);
 #endif
